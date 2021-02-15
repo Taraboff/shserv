5797612c567a58e76fbc761a8cd1b1eaca13c972
Taraboff <v.myachin@gmail.com>
2021-02-15 14:33:03 +0600

    Добавлена функция чтения из дефолтного файла контактов

--- index.js
+++ index.js
@@ -17,22 +17,33 @@ app.get('/', (req, res) => {
 });
 app.post('/sv', (req, res) => {
     // let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-    // getClientAddress = function (req) {
-    //     return req.ip.split(":").pop();
-    // };
-    // const ip = getClientAddress(req);
-    // console.log(req.ip);
-    res.sendFile(path.join(__dirname, 'data/tel', `${req.ip}.json`));
+    getClientAddress = req => req.ip.split(":").pop();
+    
+    const ip = getClientAddress(req);
+
+    fs.stat(`data/tel/${ip}.json`, function(err, stat) {
+        if(err == null) {
+            // для данного ip-адреса существует файл сохранения
+            res.sendFile(path.join(__dirname, 'data/tel', `${ip}.json`));
+        } else if(err.code == 'ENOENT') {
+            // для данного ip-адреса нет файла сохранения, отдаем файл-шаблон
+            res.sendFile(path.join(__dirname, 'data/tel', `data.json`));
+        } else {
+            console.log('Прочие причины ошибок: ', err.code);
+        }
+    });
+
 });
 
 app.post('/save', jsonParser, (req, res) => {
+    const ip = req.ip.split(':').pop();
 
-    fs.writeFile(`data/tel/${req.ip}.json`, JSON.stringify(req.body), (err) => {
+    fs.writeFile(`data/tel/${ip}.json`, JSON.stringify(req.body), (err) => {
         if (err) {
             return console.log('Ошибка записи файла');
         }
     });
-    res.json({message: `Данные сохранены в файл ${req.ip}.json`});
+    res.json({message: `Данные сохранены в файл ${ip}.json`});
 });
 
 app.listen(PORT, () => {
