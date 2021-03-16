7fe4a09c8db1f6ed058644805d0c39231189b9dd
Taraboff <v.myachin@gmail.com>
2021-03-16 17:17:55 +0600

    В процессе реализации загрузки файла

--- routes/leanRoute.js
+++ routes/leanRoute.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+
+const storageConfig = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname);
+    }
+});
+
+app.use(multer({storage:storageConfig}).single("filedata"));
+app.post("/upload", function (req, res, next) {
+   
+    let filedata = req.file;
+    if(!filedata)
+        res.send("Ошибка при загрузке файла");
+    else
+        res.send("Файл загружен");
+});
 
 router.post('/upload', (req, res) => {
     const ip = req.ip.split(":").pop();
